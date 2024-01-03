b582b64e89b08691eb2bd135cdd5cae43a94d434
siyad7siyad <muhammedsiyad17127@gmail.com>
2024-01-03 15:05:26 +0530

    host

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const dbConnection = async () => {
   try {
-    await mongoose.connect('mongodb+srv://siyad:7736711807@cluster0.izl9rfg.mongodb.net/e-commerce');
+    await mongoose.connect(process.env.DB_CONNECT);
     console.log('Connected to MongoDB');
   } catch (err) {
     console.error(err.message);
